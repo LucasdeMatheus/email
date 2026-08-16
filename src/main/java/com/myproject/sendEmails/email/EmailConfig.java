04e643623b008a51dd3d4a6f668e08c96e139aa7
Lucas <134244848+LucasdeMatheus@users.noreply.github.com>
2026-08-15 23:06:06 -0300

    Update EmailConfig.java

--- src/main/java/com/myproject/sendEmails/email/EmailConfig.java
+++ src/main/java/com/myproject/sendEmails/email/EmailConfig.java
@@ -12,30 +12,23 @@ public class EmailConfig {
     private String username;
     private String password;
     private String host = "smtp.gmail.com";
-    private int port = 587;
+    private int port = 465; // Mudar para 465 (SSL)
     private boolean auth = true;
-    private boolean starttls = true;
-    private boolean debug = false;
+    private boolean debug = true;
 
-    public EmailConfig() {
-    }
+    public EmailConfig() {}
 
-    // GETTERS
     public String getUsername() { return username; }
-    public String getPassword() { return password; }
-    public String getHost() { return host; }
-    public int getPort() { return port; }
-    public boolean isAuth() { return auth; }
-    public boolean isStarttls() { return starttls; }
-    public boolean isDebug() { return debug; }
-
-    // SETTERS
     public void setUsername(String username) { this.username = username; }
+    public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
+    public String getHost() { return host; }
     public void setHost(String host) { this.host = host; }
+    public int getPort() { return port; }
     public void setPort(int port) { this.port = port; }
+    public boolean isAuth() { return auth; }
     public void setAuth(boolean auth) { this.auth = auth; }
-    public void setStarttls(boolean starttls) { this.starttls = starttls; }
+    public boolean isDebug() { return debug; }
     public void setDebug(boolean debug) { this.debug = debug; }
 
     public Properties toProperties() {
@@ -43,13 +36,17 @@ public class EmailConfig {
         props.put("mail.smtp.host", host);
         props.put("mail.smtp.port", String.valueOf(port));
         props.put("mail.smtp.auth", String.valueOf(auth));
-        props.put("mail.smtp.starttls.enable", String.valueOf(starttls));
-        
-        // Propriedades essenciais para autenticação no Gmail
-        props.put("mail.smtp.user", username != null ? username : "");
+
+        // Habilita SSL nativo (porta 465)
+        props.put("mail.smtp.ssl.enable", "true");
         props.put("mail.smtp.ssl.trust", host);
         props.put("mail.smtp.ssl.protocols", "TLSv1.2 TLSv1.3");
-        
+
+        // Timeouts para não travar no Render
+        props.put("mail.smtp.connectiontimeout", "10000");
+        props.put("mail.smtp.timeout", "10000");
+        props.put("mail.smtp.writetimeout", "10000");
+
         return props;
     }
 }
